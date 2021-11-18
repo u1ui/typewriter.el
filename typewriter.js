1e82456cd1766bb0bca4d3a4fe72546f9bc39459
Tobias Buschor <tobias.buschor@shwups.ch>
2021-11-18 13:37:01 +0100

    cleanup

--- typewriter.js
+++ typewriter.js
@@ -27,7 +27,6 @@ u1-typewriter .-Caret {
 document.head.prepend(style);
 
 
-
 class Typewriter extends HTMLElement {
     constructor() {
         super();
@@ -43,12 +42,16 @@ class Typewriter extends HTMLElement {
             entries[0].isIntersecting ? this.play() : this.pause();
         });
 
-        this.reset();
     }
 
     static get observedAttributes() { return ['autoplay', 'loop'] }
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === 'autoplay') this.intersectionObserver[newValue===null?'unobserve':'observe'](this);
+    }
+
+
+    connectedCallback() {
+        this.reset();
     }
 
     play() {
@@ -92,7 +95,7 @@ class Typewriter extends HTMLElement {
 customElements.define('u1-typewriter', Typewriter);
 
 
-// find the next Char in children or a sibling of the node but not outside the root
+// find the next Char in children or sibling of the node but not outside the root
 function findNextChar(root, node) {
     let next = null;
     if (node.classList.contains('-Symbol')) {
@@ -101,15 +104,14 @@ function findNextChar(root, node) {
         next = node.firstElementChild || node.nextElementSibling;
     }
     if (!next) {
-        while(1) {
+        while (1) {
             node = node.parentNode;
-            if (node === null) return null;
-            if (node === root) return null;
+            if (node === null || node === root) return null;
             next = node.nextElementSibling
             if (next) break;
         }
     }
-    if (!next) return null;
+    //if (!next) return null; // not needed?
 
     if (next.classList.contains('-Symbol')) {
         return next;
@@ -117,7 +119,6 @@ function findNextChar(root, node) {
         return findNextChar(root, next);
     }
 }
-
 
 function separateChars(parent) {
     for (let node of Array.from(parent.childNodes)) {
@@ -137,83 +138,3 @@ function separateChars(parent) {
         }
     }
 }
-
-
-
-
-
-/*
-class counter extends HTMLElement {
-    constructor() {
-        super();
-
-        this._start = 0;
-        this._end = Number(this.innerHTML.replace("'",''));
-
-        this.animatedValue = 0;
-
-        this._observer = new IntersectionObserver((entries)=>{
-            entries[0].isIntersecting ? this._animate(this._start, this._end) : this._reset();
-        });
-    }
-    // set value
-    static get observedAttributes() { return ['value', 'from', 'no-grouping'] }
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (name === 'value') this.value = newValue;
-        if (name === 'from') this._start = parseFloat(newValue);
-        console.log(this._start)
-        if (name === 'no-grouping') this.noGrouping = newValue!==null;
-    }
-    set value(value){
-        let [ integer, digits='' ] = value.trim().split('.');
-        this._minDigits = digits.length;
-
-        this._end = Number(value);
-        // todo: recalculate finalWidth
-        this._animate(this.animatedValue, this._end);
-    }
-
-    connectedCallback() {
-        // measure final-width
-        this.innerHTML = format(this, this._end);
-        let widthPx = this.offsetWidth;
-        const fontSizePx = Number(getComputedStyle(this).getPropertyValue('font-size').slice(0,-2));
-        const em = widthPx / fontSizePx;
-        this.style.setProperty('--finalWidth', em+'em');
-
-        this._reset();
-
-        this._observer.observe(this);
-    }
-    disconnectedCallback() {
-        this._observer.disconnect(this)
-    }
-    _animate(from, to) { // todo easing
-        const duration = 1000;
-        const frames = Math.ceil(duration / 16);
-        let step = (to - from) / frames;
-        this._stop();
-        this.animatedValue = from;
-        this._interval = setInterval(()=>{
-            this.animatedValue += step;
-            if (step>0 ? this.animatedValue>=to : this.animatedValue<=to) {
-                this.animatedValue = to;
-                this._stop();
-            }
-            this._draw()
-        },15)
-    }
-    _stop() {
-        clearInterval(this._interval);
-    }
-    _reset() {
-        this._stop();
-        this.animatedValue = this._end;
-        this._draw();
-    }
-    _draw(){
-        requestAnimationFrame(()=> this.innerHTML = format(this, this.animatedValue) );
-    }
-    //customProperty(property) { return getComputedStyle(this).getPropertyValue('--u1-carousel-' + property); }
-}
-*/
